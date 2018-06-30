9e1e6ca8190300711919e9f336c594ea43de1b51
nakanBaka <39820938+nakanBaka@users.noreply.github.com>
2018-06-30 00:42:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,5 +11,16 @@ client.on('message', message => {
   	}
 });
 
+client.on("message", message => {
+  if(message.content == '<@430150354911494166>'){
+var embedz = new Discord.RichEmbed()
+.setAuthor('O-oi eu sou o Neko', message.author.displayAvatarURL)
+.setColor(message.guild.member(message.author.id).displayHexColor)
+.setDescription('Minha prefixo é B! para saber de mais digite B!help')
+.setTimestamp()
+.setFooter('NEKO')
+message.channel.send({embed : embedz})
+}})
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
