8ef57b71b2d49cca03dfe1a8d9a8ee8d352c3e12
nakanBaka <39820938+nakanBaka@users.noreply.github.com>
2018-06-30 00:38:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config.json');
-const fs = require('fs');
- 
-fs.readdir("./events/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    let eventFunction = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-client.on(eventName, (...args) => eventFunction.run(client, ...args));
-  });
+
+client.on('ready', () => {
+    console.log('I am ready!');
 });
-client.on("message", message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(config.prefix)) return;
- 
-  let command = message.content.split(" ")[0];
-  command = command.slice(config.prefix.length);
- 
-  let args = message.content.split(" ").slice(1);
-  // The list of if/else is replaced with those simple 2 lines:
- 
-  try {
-    let commandFile = require(`./commands/${command}.js`);
-    commandFile.run(client, message, args);
-  } catch (err) {
-    console.error(err);
-  }
- 
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
-client.on("message", message => {
-  if(message.content == '<@430150354911494166>'){
-var embedz = new Discord.RichEmbed()
-.setAuthor('O-oi eu sou o Neko', message.author.displayAvatarURL)
-.setColor(message.guild.member(message.author.id).displayHexColor)
-.setDescription('Minha prefixo é B! para saber de mais digite B!help')
-.setTimestamp()
-.setFooter('NEKO')
-message.channel.send({embed : embedz})
-}})
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
