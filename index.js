1df53ec7e528b1e99c5c42852e6066e77a33a2da
nakanBaka <39820938+nakanBaka@users.noreply.github.com>
2018-06-30 00:14:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity('https://github.com/nakanBaka/nakan-s-bot', {type: 'LISTENING'});
+    console.log('I am ready!');
 });
 
-client.on('message', msg => {
-    if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
-    const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
-    const args = msg.content.split(' ').slice(1).join(' ');
-    if (command === 'guide') return msg.channel.send('oi');
-    else if (command === 'invite') return msg.channel.send(process.env.INVITE);
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
