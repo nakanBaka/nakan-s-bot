0c22002e523d18f3e7b3315c0853558a08eed719
Nakan <39820938+nakanBaka@users.noreply.github.com>
2018-06-30 19:07:21 -0300

    Create lembrete.js

---
+++ commands/lembrete.js
@@ -0,0 +1,36 @@
+const Discord = require("discord.js");
+const ms = require("ms");
+
+module.exports.run = async (bot, message, args) => {
+
+      let reminderTime = args[0];
+      if(!reminderTime) return message.channel.send("**Diga quando devo lhe avisar, exemplo: `B!lembrete 15min tomar café**");
+
+      let reminder = args.slice(1).join(" ");
+
+      let remindEmbed = new Discord.RichEmbed()
+      .setColor('#ffffff')
+      .setAuthor(`${message.author.username}`, message.author.displayAvatarURL)
+      .addField("motivo", `\`\`\`${reminder}\`\`\``)
+      .addField("tempo", `\`\`\`${reminderTime}\`\`\``)
+      .setTimestamp();
+
+      message.channel.send(remindEmbed);
+
+
+      setTimeout(function(){
+        let remindEmbed = new Discord.RichEmbed()
+        .setColor('#ffffff')
+        .setAuthor(`${message.author.username}`, message.author.displayAvatarURL)
+        .addField("Lembrete", `\`\`\`${reminder}\`\`\``)
+        .setTimestamp()
+
+        message.author.send(remindEmbed);
+      }, ms(reminderTime));
+
+
+    }
+
+    module.exports.help = {
+      name: "remind"
+    }
