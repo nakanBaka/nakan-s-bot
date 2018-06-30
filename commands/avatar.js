865600750dea1e883345a7981b00e4f0586e29bd
nakanBaka <39820938+nakanBaka@users.noreply.github.com>
2018-06-30 15:34:15 -0300

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,23 +1,31 @@
+const Discord = require("discord.js"); //YOU WILL NEED TO DEFINE DISCORD SINCE WE ARE USING THE EMBED!
 
-const Discord = require("discord.js");
-exports.run = (client,message,args)=>{
-let member = message.mentions.users.first() || message.author;
-    
-    // Form Embed
-  let rank = member.displayAvatarurl
-    const embed = new Discord.RichEmbed()
-    
-        .setColor(0xffffff) // This will set the embed sidebar color
-        .setTitle(member.username, "aqui está se avatar!") // This will set the embed title
-       .setImage(member.displayAvatarURL)// This will set the embed image
-    .setFooter("Para de tomar café nakan!")
-        
-    // Send Message
-    message.channel.send(embed)
-    
+exports.run = async (bot, message, args) => { //OH BTW BOT IS YOUR CLIENT SO IF YOU USED CLIENT THEN CHNAGE ALL THE "BOT" TO "CLIENT" cool!
+
+    var footertext = [`${bot.user.username}: oof `, `${bot.user.username}: 🔥`, `${bot.user.username}: O tempo ta bão né ${message.author.id}`, `${bot.user.username}: to pensando em matar todos os humanos...`, `${bot.user.username}: O nakan não para de tomar café...`];
+    var rand = Math.floor(Math.random() * footertext.length);
+    var randomfooter = footertext[rand]; //THIS AND THE TWO LINES ABOVE IS TOTALLY UNNECESSARY. But you want to make your bot more interactive so keep it.Just an array of some random shit, you can add more if you would like just read the code and change it at your will! 
+
+
+    message.channel.startTyping(); // TELLS YOUR HANDICAPPED BOT TO START TYPING! ;)
+
+
+    let msg = await message.channel.send('``Gerando seu avatar sexy... Isso pode demorar um ano``') //UNNECESSARY BUT COOL.
+
+    let user = message.mentions.users.first() || message.author; //THIS IS IMPORTANT BECAUSE YOU WANT YOUR BOT TO SHOW OTHER PEOPLE'S AVATAR AS WELL BY MENTIONING THEM!
+
+    // AVATAR EMBED
+
+    let embed = new Discord.RichEmbed() //HERE WE DEFINE THE EMBED
+        .setAuthor(`${user.username} ta aqui teu avatar`) //HERE WE SHOW THE USER'S NAME!
+        .setImage(user.displayAvatarURL) // USER'S AVATAR
+        .setColor(msg.guild.me.highestRole.color) //SET THE EMBED COLOR TO THE HIGHEST ROLE COLOR THE BOT HAS! cool right :D
+        .setFooter(`${randomfooter}`) //FOOTER AND ICON
+        .setTimestamp(); //SHOWS THAT COOL TIME ON THE FOOTER!
+
+    await message.channel.send(embed) //NOW WE GIVE IT SOMETIME TO DO ALL THE CRAZY STUFF ON TOP AND THEN SEND THE EMBED!
+
+    message.channel.stopTyping(true); // TELLS YOUR HANDICAPPED BOT TO STOP TYPING! ;)
+
+    msg.delete(); // THIS WILL DELETE (Generating that sexy avatar...) AFTER SENDING THE EMBED! This will be quick so watch out for the small details :P
 }
-
-
-    module.exports.help = {
-      name: "avatar"
-    }
