6bb9baa49ba2d3e80fbbccad3d957b5a6ec76df1
Nakan <monstercat175@gmail.com>
2018-09-01 16:57:03 -0300

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -3,9 +3,6 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   let online = message.guild.members.filter(member => member.user.presence.status !== 'offline');
-  let day = message.guild.createdAt.getDate()
-  let month = 1 + message.guild.createdAt.getMonth()
-  let year = message.guild.createdAt.getFullYear()
    let sicon = message.guild.iconURL;
    let serverembed = new Discord.RichEmbed()
    .setAuthor(message.guild.name, sicon)
@@ -14,7 +11,7 @@ module.exports.run = async (bot, message, args) => {
    .setFooter(`Pedido por ${message.author.username}`)
    .addField("ID", message.guild.id, true)
    .addField("Nome", message.guild.name, true)
-   .addField(`Servidor criado em`, `${day} do ${month} de ${year}`)
+   .addField(`Servidor criado em`, `${moment.utc(message.guild.createdAt).format("LL")}`, true`)
    .addField("Dono", message.guild.owner.user.tag, true)
    .addField("Região", message.guild.region, true)
    .addField("Canais", message.guild.channels.size, true)
