30574d9e84680158875632b9e8257d2bc7d8b551
Nakan <monstercat175@gmail.com>
2018-08-31 23:57:23 -0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -3,18 +3,19 @@ exports.run = (client,message,args)=>{
 
     var usuario = message.mentions.users.first();
     if(!message.guild.member(message.author.id).hasPermissions("KICK_MEMBERS")) return message.reply("você não tem permissão de usar esse comando!")
-    if(message.mentions.users.size < 1) return message.reply("mencione alguem para kickar!")
-    if(!message.guild.member(usuario).kickable) return message.reply("eu não posso kickar essa pessoa!")
-    if(razão.length < 1) return message.reply("coloque um motivo para kickar ele")
+    if(message.mentions.users.size < 1) return message.reply("**Mencione alguem para expulsar!**")
+                                                             
+    if(!message.guild.member(usuario).kickable) return message.reply("**Eu não posso expulsar esta pessoa!**")
+    if(razão.length < 1) return message.reply("**Coloque um motivo para expulsar o usuário!**")
 
     message.guild.member(usuario).kick()
 
    var discord = require ('discord.js')
 
    var embed = new discord.RichEmbed()
-   .setTitle("usario expulso do servidor! :o")
-   .addField("usuario:",usuario.username)
-   .addField("razão:", razão);
+   .setTitle("**Usuário expulso do servidor!**")
+   .addField("**Usuário:**",usuario.username)
+   .addField("**Motivo:**", razão);
 
    message.channel.send(embed)
 }
