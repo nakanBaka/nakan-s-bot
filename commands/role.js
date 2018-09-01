31bbf5204cbe74b406889ce86c4fd97fbf7301c6
Nakan <monstercat175@gmail.com>
2018-09-01 17:54:06 -0300

    Create role.js

---
+++ commands/role.js
@@ -0,0 +1,31 @@
+exports.run = async (client, message, args, level) => {
+    var user = message.mentions.members.first(); //User
+    var roleName = args.splice(2).join(' '); //Role Name
+    var role = message.guild.roles.find('name', roleName); //Role Search
+
+
+    switch (args[0]) {
+        case 'add':
+        if (!message.member.hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return message.reply("Desculpe mas você não possui permissão para adicionar cargos.");    
+        if (!user) return message.reply('Você precisa mencionar um usuário válido deste servidor'); // I need User
+           //I need roleName
+            if (!message.guild.roles.find('name', roleName)) return message.reply('Nenhum cargo com este nome existe. _Os nomes de cargos fazem distinção entre maiúsculas e minúsculas_');
+            if (user.roles.exists('name', roleName)) return message.reply(':eyes: Eu vejo esse cargo neste usuário já!'); //Already have role
+
+
+            user.addRole(role).then(() => message.reply('Cargo adicionado')).catch((err) => message.reply('Não é possível adicionar cargo').then(() => console.log(err)));
+            break;
+        case 'remove':
+        if (!message.member.hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return message.reply("Desculpe mas você não possui permissão para remover cargos.")
+            if (!user) return message.reply('Você precisa mencionar um usuário válido deste servidor');
+            //console.log(user);
+            //console.log(roleName);
+            if (!message.guild.roles.find('name', roleName)) return message.reply('Nenhum cargo com este nome existe. _Os nomes de cargos fazem distinção entre maiúsculas e minúsculas._');
+
+            if (!user.roles.find('name', roleName)) return message.reply('Esse usuário ainda tem o papel?');
+
+            user.removeRole(role).then(() => message.reply('Cargo removido')).catch((err) => message.reply('Não é possível remover o cargo').then(() => console.log(err)));
+            break;
+        default:
+            message.reply('Bem, você pode adicionar ou remover papéis... **B!role add**, para adicionar papéis ou **B!role remove**, para tirar papéis.');
+    }}
