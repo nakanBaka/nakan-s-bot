6f43a41b28fc28ba269755b69ebdfe0446cef18b
Nakan <monstercat175@gmail.com>
2018-09-04 11:57:43 -0300

    Update anunciar.js

--- commands/anunciar.js
+++ commands/anunciar.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 module.exports.run = async (client, message, args) => {
 
 
-if(!message.member.hasPermission("ADMINISTRATOR")) return;
+if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("**Sem permissão!**");
 const sayMessage = args.join(" ");
 message.delete()
 if(!sayMessage)return message.reply("**Diga algo para anunciar**");
