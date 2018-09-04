9ef71d56bd37ccfc148d67d7d31903e0dd9b83c9
Nakan <monstercat175@gmail.com>
2018-09-04 00:25:57 -0300

    Create anunciar.js

---
+++ commands/anunciar.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+
+
+module.exports.run = async (client, message, args) => {
+
+
+if(!message.member.hasPermission("ADMINISTRATOR")) return;
+const sayMessage = args.join(" ");
+message.delete()
+
+const embed = new Discord.RichEmbed()
+
+.setTitle('📢 **Anuncio!** 📢')
+.setDescription(sayMessage)
+.setFooter(`Enviado por: ${message.author.username}`)
+.setTimestamp(new Date())
+.setColor('RANDOM')
+.setThumbnail(message.guild.iconURL);
+
+
+message.channel.send(embed);
+
+}
