4ce6b06cbeac5d8e135d1df4e712276a0872d260
Nakan <monstercat175@gmail.com>
2018-09-05 19:57:36 -0300

    Update clean.js

--- commands/clean.js
+++ commands/clean.js
@@ -3,6 +3,7 @@ exports.run = (client, message, args)  => {
       if (message.author.bot) return message.reply("**Bots não podem usar esse comando!**")
       if (!message.member.hasPermission(["MANAGE_MESSAGES"])) return message.reply("**Você não tem permissão para limpar o chat!**");
       if (reason.length < 1) return message.reply('**Diga a quantidade de mensagens que devo apagar!**');
+      message.delete()
       message.channel.bulkDelete(`${args[0]}`)
         setTimeout(function() {
             message.channel.sendMessage(`** ${args[0]} mensagens Foram limpadas por <@${message.author.id}>!**`).then((value) => {
