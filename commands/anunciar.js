54c993f0f26a58c8df08fa68e24033026544c423
Nakan <monstercat175@gmail.com>
2018-09-08 16:30:44 -0300

    Update anunciar.js

--- commands/anunciar.js
+++ commands/anunciar.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message, args) => {
 if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("**<:xCharlie:488064590811234304> Sem permissão!**");
 const sayMessage = args.join(" ");
  message.delete()
-if(!sayMessage)return message.reply("**Diga algo para anunciar**");
+if(!sayMessage)return message.reply("**<:xCharlie:488064590811234304> Diga algo para anunciar**");
  
 const embed = new Discord.RichEmbed()
 
