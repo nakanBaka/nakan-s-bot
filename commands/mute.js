3fa070a4efab5ee0cc5f08245a81e714a9a25e94
Nakan <monstercat175@gmail.com>
2018-09-15 14:07:09 -0300

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -3,11 +3,11 @@ const ms = require("ms");
 
 module.exports.run = async (bot, message, args) => {
     let user = message.mentions.users.first()
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Desculpe mas você não possui permissão.");
-    if(message.mentions.users.size < 1) return message.reply("Você não mencionou ninguém!")
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("**Desculpe mas você não possui permissão.**");
+    if(message.mentions.users.size < 1) return message.reply("**Você não mencionou ninguém!**")
     let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if (!tomute) return message.reply("Não achei o usuario.");
-    if (tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Este usuario tem o mesmo cargo que você, ou um cargo maior que você.");
+    if (!tomute) return message.reply("**Não achei o usuario.**");
+    if (tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("**Não posso mutar este usuário.**");
     let muterole = message.guild.roles.find(`name`, "Charlie-Mute");
 
     if (!muterole) {
@@ -29,14 +29,14 @@ module.exports.run = async (bot, message, args) => {
     }
 
     let mutetime = args[1];
-    if (!mutetime) return message.reply("Você não especificou o tempo!");
+    if (!mutetime) return message.reply("**Você não especificou o tempo!**");
 
     await (tomute.addRole(muterole.id));
     message.reply(`<@${tomute.id}> foi mutado por ${ms(ms(mutetime))}`);
-console.log(`${message.author.username} MUTOU brutalmente ${user.username}`)
+console.log(`${message.author.username} Mutou: ${user.username}`)
     setTimeout(function() {
         tomute.removeRole(muterole.id);
-        console.log(`<@${tomute.id}> já foi desmutado!`);
+        message.channel.send(`<@${tomute.id}> **Foi desmutado pelo tempo!**`);
     }, ms(mutetime));
 
 }
