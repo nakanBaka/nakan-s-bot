a4b9ff9f3aa8a15c5efb749af995a519e294f934
Nakan <monstercat175@gmail.com>
2018-09-23 16:02:32 -0300

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -4,12 +4,11 @@ const fs = require("fs")
 exports.run = (Link, message, args) => {
   var desenvolvedores = ["341372896922435595"]
   if (!desenvolvedores.includes(message.author.id) & !message.member.hasPermission(["MANAGE_ROLES_OR_PERMISSIONS"])) return;
-if(message.content.startsWith(config.prefix + "prefix")) {
-    if (!args[0]) return message.channel.send(":error: **Prefix not inserted!**")
+    if (!args[0]) return message.channel.send(":error: **Diga para que prefixo devo mudar!**")
     let newPrefix = message.content.split(" ").slice(1, 2)[0];
 
     config.prefix = newPrefix;
-    message.channel.send(`📘 Prefix changed to \`${newPrefix}\``).catch(console.error);
+    message.channel.send(`📘 Minha prefixo agora é: \`${newPrefix}\``).catch(console.error);
   
     fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
   }
