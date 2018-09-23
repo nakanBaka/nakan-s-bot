6b16ad63ec59fba197da4e056113ee30066cc189
Nakan <monstercat175@gmail.com>
2018-09-23 13:50:24 -0300

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -5,7 +5,7 @@ exports.run = (Link, message, args) => {
   var desenvolvedores = ["341372896922435595"]
   if (!desenvolvedores.includes(message.author.id) & !message.member.hasPermission(["MANAGE_ROLES_OR_PERMISSIONS"])) return;
 if(message.content.startsWith(config.prefix + "prefix")) {
-    if (!args[0]) return message.channel.send(":error: **Diga para que prefix devo mudar!**")
+    if (!args[0]) return message.reply(":error: **Diga para que prefix devo mudar!**")
     let newPrefix = message.content.split(" ").slice(1, 2)[0];
 
     config.prefix = newPrefix;
