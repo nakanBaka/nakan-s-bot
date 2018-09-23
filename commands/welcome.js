014b1b55a0ecd272e9e89fa4428da363f2e5ee6c
Nakan <monstercat175@gmail.com>
2018-09-23 15:06:06 -0300

    Update welcome.js

--- commands/welcome.js
+++ commands/welcome.js
@@ -9,7 +9,7 @@ exports.run = (client, message, args) => {
     
           if (!message.member.hasPermission(["MANAGE_GUILD"])) return message.reply("**Você não tem permissão para definir meu welcome!**")
 
-    database.Guilds.findOne({
+   database.Guilds.findOne({
         "_id": message.guild.id
     }, function(erro, documento) {
 
@@ -17,39 +17,34 @@ exports.run = (client, message, args) => {
 
             if (!razaou.length < 1) {
 
-                if (message.content.startsWith("c!welcome define")) {
+                if (message.content.startsWith("c!welcome set")) {
                     if (!razaod.length < 1) {
 
                         documento.welcomechannel = message.channel.id
-                        documento.welcomemsg = message.content.replace("c!welcome define ", "");
+                        documento.welcomemsg = message.content.replace("c!welcome set ", "");
                         documento.welcome = true
                         documento.save();
-                        message.reply("**Welcome definido!**");
+                        message.reply("**Welcome setado com sucesso!**");
 
                     } else {
                         message.channel.sendMessage({
                             "embed": {
-                                "description": "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ**Bem-vindo**ㅤㅤㅤㅤㅤㅤㅤㅤ\nㅤ\n**Como usar:**\n```c!welcome set <mensagem de boas-vindas>```",
-                                "color": 55512,
-                                "timestamp": new Date(),
-                                "footer": {
-                                  "icon_url": message.author.displayAvatarURL,
-                                  "text": message.author.username
-                                },
-                                   }
-                        })
+                                "description": "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ**Bem-vindo**ㅤㅤㅤㅤㅤㅤㅤㅤ\nㅤ\n**Como usar:**\n```c!welcome set <mensagem de boas-vindas>.",
+                                "color": 55512
+                            }
+                        });
                     }
-                
+                }
 
                 if (message.content.startsWith("c!welcome delete")) {
                     if (!documento.welcome) {
-                        message.reply("**Não há nenhuma mensagem de boas vindas feita por mim neste servidor!**");
+                        message.reply("**Não há um welcome definido neste servidor!**");
                     } else {
                         documento.welcome = false
                         documento.welcomechannel = "Nenhum"
                         documento.welcomemsg = "Nenhuma"
                         documento.save()
-                        message.reply("**Welcome deletado!**");
+                        message.reply("**Welcome removido com sucesso!**");
                     }
                 }
 
@@ -57,26 +52,15 @@ exports.run = (client, message, args) => {
                     if (!documento.welcome) {
                         message.channel.sendMessage({
                             "embed": {
-                                "description": "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ**Bem-vindo**ㅤㅤㅤㅤㅤㅤㅤㅤ\nㅤ\n**Como usar:**\n```{user} menciona o usuário\n{server} fala o nome do servidor\n{tag} fala o nome do usuário```",
-                                "color": 55512,
-                                "timestamp": new Date(),
-                                "footer": {
-                               }
+                                "description": "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ**❄ WELCOME ❄**ㅤㅤㅤㅤㅤㅤㅤㅤ\nㅤ\n**Como usar:**\n```{member} menciona o usuário\n{guild} fala o nome do servidor\n{name} fala o nome do usuário```",
+                                "color": 55512
                             }
                         });
-                    }
-
-                }
                     } else {
                         message.channel.sendMessage({
                             "embed": {
                                 "description": "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ**Bem-vindo**ㅤㅤㅤㅤㅤㅤㅤㅤ\nㅤ\n**Mensagem de boas vindas:** " + documento.welcomemsg + "\n**Canal de boas vindas:** <#" + documento.welcomechannel + ">\nㅤ\n**Como usar:**\n```{user} menciona o usuário\n{server} fala o nome do servidor\n{tag} fala o nome do usuário```",
-                                "color": 55512,
-                                "timestamp": new Date(),
-                                "footer": {
-                                  "icon_url": message.author.displayAvatarURL,
-                                  "text": message.author.username
-                                 }
+                                "color": 55512
                             }
                         });
                     }
@@ -87,16 +71,12 @@ exports.run = (client, message, args) => {
                 message.channel.sendMessage({
                     "embed": {
                         "description": "ㅤㅤㅤㅤㅤㅤㅤㅤㅤ**Ajuda**ㅤㅤㅤㅤㅤㅤ\nㅤ\n**Como usar:**\n```c!welcome define\c!welcome delete\c!welcome info```",
-                        "color": 55512,
-                        "timestamp": new Date(),
-                        "footer": {
-                          "icon_url": message.author.displayAvatarURL,
-                          "text": message.author.username
-                       }
+                        "color": 55512
                     }
                 });
 
             }
+
         } else {
 
             var servidor = new database.Guilds({
