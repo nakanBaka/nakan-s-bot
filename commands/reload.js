a1d0f7e1539978873b3d795ad72c76402fbc7874
Nakan <monstercat175@gmail.com>
2021-06-22 13:00:09 -0300

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -1,5 +1,5 @@
 exports.run = async (client, message, args) => {
-    if (message.author.id === '341372896922435595') {
+    if (message.author.id === '427898378123214858') {
         if (!args[0]) return message.channel.send("**Digite o nome do comando que quer recarregar.**");
         delete require.cache[require.resolve(`./${args[0]}.js`)];
         message.channel.send(`**O comando ${args[0]} foi recarregado.**`);
