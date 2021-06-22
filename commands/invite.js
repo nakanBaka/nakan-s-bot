6a7eeae4d0a5c34f53991052b615e4b6a188fc31
Nakan <monstercat175@gmail.com>
2021-06-22 12:56:38 -0300

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
 var embedz = new Discord.RichEmbed()
 .setAuthor(message.author.username,message.author.displayAvatarURL)
 .setColor(message.guild.member(message.author.id).displayHexColor)
-.setDescription(`**Olá! Vejo que quer me adicionar ao seu servidor! É claro que pode, clique [aqui](https://discordapp.com/api/oauth2/authorize?client_id=484534602644455456&permissions=8&scope=bot) para me adicionar com permissão de administrador.**`)
+.setDescription(`**Olá! Vejo que quer me adicionar ao seu servidor! É claro que pode, clique [aqui](https://discord.com/api/oauth2/authorize?client_id=856914967491248178&permissions=0&scope=bot) para me adicionar com permissão de administrador.**`)
 .setTimestamp()
 .setFooter(`${message.author.username}`)
 message.channel.send({embed : embedz})
