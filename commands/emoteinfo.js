114c77e24b50fff840e75f5990c2e265a34d0ff3
Nakan <monstercat175@gmail.com>
2021-06-22 13:37:20 -0300

    Update emoteinfo.js

--- commands/emoteinfo.js
+++ commands/emoteinfo.js
@@ -4,20 +4,13 @@ moment.locale("pt-BR")
 
 module.exports.run = async (bot, message, args) => {
         let emoji = message.guild.emojis.find(emoji => emoji.name === `${args.join(" ")}`)
-        let animado;
-            if (emoji.animated === true) animado = "**Sim!**"
-            if (emoji.animated === false) animado = "**Não!**"
-        let gerenciadotwitch;
-            if (emoji.managed === true) gerenciadotwitch = "**Sim**"
-            if (emoji.managed === false) gerenciadotwitch = "**Não**"
+        
         const embed = new Discord.RichEmbed()
         .setAuthor(`Informações do emoji ${emoji.name}`)
         .setColor("#FF0000")
         .setThumbnail(emoji.url)
         .addField("Do servidor:", emoji.guild.name)
-        .addField("Animado?", animado)
         .addField("Criado em:", moment(emoji.createdAt).format("LLLL"))
         .addField("ID:", emoji.id)
-        .addField("Gerenciado pela Twitch:", gerenciadotwitch)
         message.channel.send(embed)
 }
