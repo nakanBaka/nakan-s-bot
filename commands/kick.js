70a8b7570af8897be671cc57c2e7181ed6ecbdd0
Nakan <monstercat175@gmail.com>
2021-06-22 12:58:33 -0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -2,11 +2,11 @@ exports.run = (client,message,args)=>{
     var razão = args.slice(1).join(" ")
 
     var usuario = message.mentions.users.first();
-    if(!message.guild.member(message.author.id).hasPermissions("KICK_MEMBERS")) return message.reply(" <:xCharlie:488064590811234304> **você não tem permissão de usar esse comando!**")
-    if(message.mentions.users.size < 1) return message.reply(" <:xCharlie:488064590811234304> **Mencione alguem para expulsar!**")
+    if(!message.guild.member(message.author.id).hasPermissions("KICK_MEMBERS")) return message.reply("**você não tem permissão de usar esse comando!**")
+    if(message.mentions.users.size < 1) return message.reply("**Mencione alguem para expulsar!**")
                                                              
-    if(!message.guild.member(usuario).kickable) return message.reply(" <:xCharlie:488064590811234304> **Eu não posso expulsar esta pessoa!**")
-    if(razão.length < 1) return message.reply(" <:xCharlie:488064590811234304> **Coloque um motivo para expulsar o usuário!**")
+    if(!message.guild.member(usuario).kickable) return message.reply("**Eu não posso expulsar esta pessoa!**")
+    if(razão.length < 1) return message.reply("**Coloque um motivo para expulsar o usuário!**")
 
     message.guild.member(usuario).kick()
 
