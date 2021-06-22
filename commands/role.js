b4da79a620eba15b8009963cf3506eefc2267212
Nakan <monstercat175@gmail.com>
2021-06-22 13:02:51 -0300

    Update role.js

--- commands/role.js
+++ commands/role.js
@@ -9,8 +9,8 @@ exports.run = async (client, message, args, level) => {
         if (!message.member.hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return message.reply("Desculpe mas você não possui permissão para adicionar cargos.");    
         if (!user) return message.reply('Você precisa mencionar um usuário válido deste servidor'); // I need User
            //I need roleName
-            if (!message.guild.roles.find('name', roleName)) return message.reply('Nenhum cargo com este nome existe. _Os nomes de cargos fazem distinção entre maiúsculas e minúsculas_');
-            if (user.roles.exists('name', roleName)) return message.reply(':eyes: Eu vejo esse cargo neste usuário já!'); //Already have role
+            if (!message.guild.roles.find('name', roleName)) return message.reply('Nenhum cargo com este nome existe. _Os nomes de cargos fazem distinção entre letras maiúsculas e minúsculas_');
+            if (user.roles.exists('name', roleName)) return message.reply('O usuário já possui este cargo!'); //Already have role
 
 
             user.addRole(role).then(() => message.reply('Cargo adicionado')).catch((err) => message.reply('Não é possível adicionar cargo').then(() => console.log(err)));
@@ -20,12 +20,12 @@ exports.run = async (client, message, args, level) => {
             if (!user) return message.reply('Você precisa mencionar um usuário válido deste servidor');
             //console.log(user);
             //console.log(roleName);
-            if (!message.guild.roles.find('name', roleName)) return message.reply('Nenhum cargo com este nome existe. _Os nomes de cargos fazem distinção entre maiúsculas e minúsculas._');
+            if (!message.guild.roles.find('name', roleName)) return message.reply('Nenhum cargo com este nome existe. _Os nomes de cargos fazem distinção entre letras maiúsculas e minúsculas._');
 
-            if (!user.roles.find('name', roleName)) return message.reply('Esse usuário ainda tem o papel?');
+            if (!user.roles.find('name', roleName)) return message.reply('Esse usuário ainda tem o cargo?');
 
             user.removeRole(role).then(() => message.reply('Cargo removido')).catch((err) => message.reply('Não é possível remover o cargo').then(() => console.log(err)));
             break;
         default:
-            message.reply('Bem, você pode adicionar ou remover papéis... **B!role add**, para adicionar papéis ou **B!role remove**, para tirar papéis.');
+            message.reply('Bem, você pode adicionar ou remover cargos... **d.role add**, para adicionar papéis ou **d.role remove**, para tirar cargos.');
     }}
